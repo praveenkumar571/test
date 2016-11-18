18bdac0e92fd6fad1c1f752c52b17cfac9a41a90
Praveen Kumar <praveenselenium09@gmail.com>
2016-11-18 23:04:48 +0530

    added new file

---
+++ SampleJava/src/TestClass - Copy.java
@@ -0,0 +1,13 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+	
+	System.out.println("out put is"+c);
+	System.out.println("added a line whether its alone commit or with the file its commit");
+
+}
+}
