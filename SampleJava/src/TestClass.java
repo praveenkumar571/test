1a87bb12bfec74fc440fc0b3f378c73e53aefbdc
Praveen Kumar <praveenselenium09@gmail.com>
2016-11-18 23:04:48 +0530

    this is first commit

---
+++ SampleJava/src/TestClass.java
@@ -0,0 +1,27 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+	
+	System.out.println("out put is"+c);
+*praveen
+
+<<<<<<< .mine
+skrjfkjfkf
+;sfk;sk;fksd
+sfjlskdjflsdjfriurfusdflldsf
+lsjflksdjfdsjf;dsf;df
+fdsjllkfsdsfdjlkfjdslkf
+sdfllkdfjjlkjklfds
+
+
+||||||| .r4
+	}
+
+
+test 
+
+}
